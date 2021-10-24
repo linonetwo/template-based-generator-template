4f8a8674ffa8b7fa24023b1cc74f0953652b9272
Lin Onetwo <linonetwo012@gmail.com>
2021-10-24 22:01:00 +0800

    fix: ln -s path found failed in the CI

--- src/data/index.ts
+++ src/data/index.ts
@@ -6,9 +6,21 @@ const importedFiles = preval`
 const fs = require('fs');
 const path = require('path');
 const filesInTemplateFolder = fs.readdirSync(path.resolve(__dirname, 'templates'));
+console.log('filesInTemplateFolder', filesInTemplateFolder);
 const results = {};
 for (const fileName of filesInTemplateFolder) {
-  results[fileName.split('.')[0]] = fs.readFileSync(path.resolve(__dirname, 'templates', fileName), 'utf8');
+  const filePath = path.resolve(__dirname, 'templates', fileName);
+  console.log('loading file', filePath);
+  try {
+    results[fileName.split('.')[0]] = fs.readFileSync(filePath, 'utf8');
+  } catch (error) {
+    // readme.md found failed in the CI
+    // Fix: [!] (plugin babel) Error: /home/runner/work/template-based-generator-template/template-based-generator-template/src/data/index.ts: preval.macro: ENOENT: no such file or directory, open '/home/runner/work/template-based-generator-template/template-based-generator-template/src/data/templates/README.md' Learn more: https://www.npmjs.com/package/preval.macro
+    if (fileName.toLowerCase() === 'readme') {
+      const fixedReadmePath = path.resolve(__dirname, '..', '..', fileName);
+      results[fileName.split('.')[0]] = fs.readFileSync(fixedReadmePath, 'utf8');
+    }
+  }
 }
 module.exports = results` as Record<string, string>;
 
